437bb6e966a8cbe2b9e373c6e4f68f61d10da08f
Braden Schaeffer <braden.schaeffer@gmail.com>
2015-10-31 15:03:26 -0400

    Add some ZeroAuth::Config docs

--- lib/zero_auth/config.rb
+++ lib/zero_auth/config.rb
@@ -1,11 +1,15 @@
 module ZeroAuth
   class Config
+    # @return [Integer] The cost param when generating BCrypt passwords.
+    #   Defaults to 9.
     attr_accessor :password_cost
 
     def initialize
       reset!
     end
 
+    # Resets the current configuration values to their defaults.
+    #
     def reset!
       self.password_cost = 9
     end
